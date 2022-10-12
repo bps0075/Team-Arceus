402d5784936bf797c2bed1f47cc40f94e2ed1d95
bpsha <bps0075>
2022-10-11 19:05:37 -0500

    Created 5 HTML folders, made views, and edited settings and urls. It now can produce a web page or 3.

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+# from django.views.generic import RedirectView
+from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    #path('catalog/', include('catalog.urls')),
+    #path('', RedirectView.as_view(url='catalog/')),
+    path('', views.index, name="index"), #Homepage
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+] # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
