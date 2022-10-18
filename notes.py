96c57ac739625116f165d3fa91cb3a075ffccc3c
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2022-10-18 00:07:44 -0500

    Delete notes.py

--- notes.py
+++
@@ -1,6 +0,0 @@
-# This is where the classes for the notes will be done
-
-def createNotes():
-    print(f'Welcome to the notes. You may add any notes to this that you want to. This is a test!')
-
-createNotes()
