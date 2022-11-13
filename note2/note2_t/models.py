58f6aec311802f2c56071a8463534860691796c1
bpsha <bps0075>
2022-11-13 17:13:32 -0600

    Filled in the blank classes, but incomplete.

--- note2/note2_t/models.py
+++ note2/note2_t/models.py
@@ -6,7 +6,20 @@ from ckeditor_uploader.fields import RichTextUploadingField
 class notes(models.Model):
     body = RichTextUploadingField()
 
-class save():
+class save(models.Model):
+    body = "something"
+    # writing all the lines to a text file
+    outputFile = open("outputFile.txt", "w")
+    for line in body:
+        outputFile.write(line)
+        outputFile.write("\n")
+    outputFile.close()
 
-# class read():
-
+class read():
+    # reading all the lines from a text tile
+    body = ""
+    readFile = open("outputFile.txt", "r")
+    for line in readFile:
+        # print(line)
+        body = body + "" + line + " "
+    readFile.close()
