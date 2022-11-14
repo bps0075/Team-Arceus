84e989185e82689f237cb689622b24d6e49dd636
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2022-11-14 15:28:15 -0600

    Adding in current save and edit files.
    These files don't work. If you get the chance, see if you can figure something out

---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py
+from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class notes(models.Model):
+    title = models.CharField(max_length=255,default="OLD")
+    body = RichTextUploadingField()
+
+    def get_absolute_url(self):
+        return reverse('home')
---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HomeView, NotesView, UpdateNotesView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name = 'home'),
+    path('notes', NotesView.as_view(), name = 'notes'),
+    path('notes/edit/<int:pk>',UpdateNotesView.as_view,name='update_notes')
+]
