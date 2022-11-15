ef6d1a5897d475d7d1797dd0894b5e907bacd277
bpsha <bps0075>
2022-11-15 14:04:36 -0600

    Removing code that did not function.

--- note2/note2_t/models.py
+++ note2/note2_t/models.py
@@ -5,21 +5,3 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class notes(models.Model):
     body = RichTextUploadingField()
-
-class save(models.Model):
-    body = "something"
-    # writing all the lines to a text file
-    outputFile = open("outputFile.txt", "w")
-    for line in body:
-        outputFile.write(line)
-        outputFile.write("\n")
-    outputFile.close()
-
-class read():
-    # reading all the lines from a text tile
-    body = ""
-    readFile = open("outputFile.txt", "r")
-    for line in readFile:
-        # print(line)
-        body = body + "" + line + " "
-    readFile.close()
