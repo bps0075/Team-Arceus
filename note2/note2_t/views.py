a6ee127694307688dc73e3927d2f0eb9b4b4cdbc
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2022-11-27 15:57:41 -0600

    Update views.py

--- note2/note2_t/views.py
+++ note2/note2_t/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import notes
 
-class HomeView(CreateView):
+class HomeView(ListView):
 
     model = notes
     template_name = 'home.html'
-    fields = '__all__'
 
 class NotesView(CreateView):
 
     model = notes
     template_name = 'notes.html'
     fields = '__all__'
+
+class UpdateNotesView(UpdateView):
+    model = notes
+    template_name = 'update_notes.html'
+    fields = ['title', 'body']
