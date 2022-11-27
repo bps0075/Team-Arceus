2630aa1fd451b7a525bce8964232141cf464f879
Ealex97 <113632208+Ealex97@users.noreply.github.com>
2022-11-27 15:57:03 -0600

    Update models.py

--- note2/note2_t/models.py
+++ note2/note2_t/models.py
@@ -2,6 +2,12 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class notes(models.Model):
+    title = models.CharField(max_length=255,default="Notes")
     body = RichTextUploadingField()
+
+    def get_absolute_url(self):
+        return reverse('home')
